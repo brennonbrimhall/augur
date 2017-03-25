70821b690011933cf9e1afeb690ee462371b73c7
Brennon Brimhall <brennonbrimhall@gmail.com>
2017-03-25 15:51:37 -0600

    Fixed bug.

--- auto.py
+++ auto.py
@@ -4,6 +4,7 @@ import argparse
 from datetime import datetime
 from urllib import FancyURLopener
 import json
+import subprocess
 
 parser = argparse.ArgumentParser(description='Compute FRC rankings simulations.')
 parser.add_argument('eventcode')
@@ -11,13 +12,13 @@ parser.add_argument('depth')
 
 args = parser.parse_args()
 
-year = datetime.now().year
-eventCode = args.eventcode
-event = year + eventCode
-depth = args.depth
+year = str(datetime.now().year)
+eventCode = str(args.eventcode)
+event = str(year + eventCode)
+depth = str(args.depth)
 
 #put into file
-file = open(year+eventCode+'.py')
+file = open(year+eventCode+'.py', 'w+')
 
 class AugurOpener(FancyURLopener):
 	version = 'brennon-brimhall:augur:2'
@@ -39,10 +40,12 @@ url = "https://www.thebluealliance.com/api/v2/event/" + event +"/stats?X-TBA-App
 response = opener.open(url)
 stats = json.loads(response.read())["oprs"]
 
-file.write("import event")
-file.write(eventCode + " = event.Event(" + depth + ")")
+file.write('#!/usr/bin/env python\n\n')
 
-file.write("") #will put in a newline
+file.write("import event\n")
+file.write(eventCode + " = event.Event(" + depth + ")\n")
+
+file.write("\n") #will put in a newline
 
 #counter to ignore the header
 i = 0;
@@ -59,10 +62,10 @@ for rank in rankings:
 		fourthSort = str(round((rank[6] / matchesPlayed), 2))
 		fifthSort = str(round((rank[7] / matchesPlayed), 2))
 
-		file.write(eventCode + ".addTeam(" + team + ",\t" + rp + ",\t" + firstSort + ",\t" + secondSort + ",\t" + thirdSort + ",\t" + fourthSort + ",\t" + fifthSort + ")")
+		file.write(eventCode + ".addTeam(" + team + ",\t" + rp + ",\t" + firstSort + ",\t" + secondSort + ",\t" + thirdSort + ",\t" + fourthSort + ",\t" + fifthSort + ")\n")
 	i += 1
 
-file.write("") #will put int a newline
+file.write("\n") #will put int a newline
 
 for match in matches:
 	matchType = match['comp_level']
@@ -79,7 +82,7 @@ for match in matches:
 	blue3 = match['alliances']['blue']['teams'][2][3:]
 
 	#Calculate probability via OPR if available; fall back to straight 50% if not.
-	prob = ".5"
+	prob = '.5'
 
 	redTotal = (stats[red1] + stats[red2] + stats[red3])
 	blueTotal = (stats[blue1] + stats[blue2] + stats[blue3])
@@ -88,10 +91,10 @@ for match in matches:
 		prob = str(round((stats[red1] + stats[red2] + stats[red3]) / (stats[red1] + stats[red2] + stats[red3] + stats[blue1] + stats[blue2] + stats[blue3]),2))
 
 	if (match['score_breakdown'] == None):
-		file.write(eventCode + ".addMatch(" + red1 + ",\t" + red2 + ",\t" + red3 + ",\t" + blue1 + ",\t" + blue2 + ",\t" + blue3 + ",\t" + prob + ")\t#Match " + matchNumber)
+		file.write(eventCode + ".addMatch(" + red1 + ",\t" + red2 + ",\t" + red3 + ",\t" + blue1 + ",\t" + blue2 + ",\t" + blue3 + ",\t" + prob + ")\t#Match " + matchNumber + "\n")
 	else:
-		file.write("#" + eventCode + ".addMatch(" + red1 + ",\t" + red2 + ",\t" + red3 + ",\t" + blue1 + ",\t" + blue2 + ",\t" + blue3 + ",\t" + prob + ")\t#Match " + matchNumber)
+		file.write("#" + eventCode + ".addMatch(" + red1 + ",\t" + red2 + ",\t" + red3 + ",\t" + blue1 + ",\t" + blue2 + ",\t" + blue3 + ",\t" + prob + ")\t#Match " + matchNumber + "\n")
 
-file.write("") #newline
-file.write(eventCode + ".calculate()")
-file.write("print\"\"")
+file.write("\n") #newline
+file.write(eventCode + ".calculate()\n")
+file.write("print \"\"\n")
